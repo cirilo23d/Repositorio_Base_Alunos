e0874abbd333dcbd14f45273d1553bd4cb6813aa
cirilo23d <cirinodaniel920@gmail.com>
2026-05-26 10:38:41 -0300

    Add files via upload

---
+++ Modulo 2/Exercicios/automacao_cli.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+#os.getcwd() - Mostra a pasta atual
+#os.listdir() - Lista arquivos e pastas
+#os.mkdir("pasta") - Cria uma pasta
+#os.remove("pasta") - Remove uma pasta
+#shutil.move("origem", "destino") - Move uma pasta da origem ao destino.
+#os.system("comando") - Executa um comando.
+
+print("CRiador de pastas")
+pasta = input("Digite o nome da pasta que deseja criar: ")
+os.mkdir(pasta)
