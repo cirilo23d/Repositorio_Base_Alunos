8803ced16e925dd892398bffa8d59f13f9a04274
cirilo23d <cirinodaniel920@gmail.com>
2026-08-04 11:49:33 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/Exercício01.py
@@ -0,0 +1,5 @@
+def dobro(numero):
+    return numero * 2
+
+print(dobro(2.5))
+print(dobro(12.5))
---
+++ Modulo 2/Manipulação de Arquivos e Automação/Exercício02.py
@@ -0,0 +1,7 @@
+def aria(base, altura):
+    return base * altura /2
+
+nuemro_b = float(input("Digite o número da base de sua esolha: "))
+nuemro_a = float(input("Digita o número da altura de sua escolha: "))
+
+print("A área do triângulo: ", aria(nuemro_b, nuemro_a))
